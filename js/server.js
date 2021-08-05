65981aeacc3d80fe6b9b03936d743b2868525111
Richard <you@example.com>
2021-08-05 19:58:23 +0300

    adding loops and conditions

--- js/server.js
+++ js/server.js
@@ -1,19 +1,42 @@
-const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+    let numberOfFilms,
+        lastFilm,
+        rateOfFilm;
 
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+    do {
+        numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+    } while (numberOfFilms == 0);
 
-const lastFilm1 = prompt("Один из последних просмотренных фильмов?", "");
-const rateOfFilm1 = prompt("На сколько оцените его?", "");
-const lastFilm2 = prompt("Один из последних просмотренных фильмов?", "");
-const rateOfFilm2 = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[lastFilm1] = rateOfFilm1;
-personalMovieDB.movies[lastFilm2] = rateOfFilm2;
+    let personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
 
-console.log(personalMovieDB);
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотренно довольно мало фильмов");
+    } else if ((personalMovieDB.count >= 10)&&(personalMovieDB.count <= 30)) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+    
+    
+    for (let i = 0; i<2; i++) {
+            lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+            rateOfFilm = prompt("На сколько оцените его?", "");
+        if (lastFilm != null && rateOfFilm != null && lastFilm != "" && rateOfFilm != "" && lastFilm.length <= 50) {
+            console.log("Done");
+            personalMovieDB.movies[lastFilm] = rateOfFilm;
+        } else {
+             console.log("Error");
+            i--;
+        }
+        //personalMovieDB.movies[lastFilm] = rateOfFilm;
+    }
+    
+    console.log(personalMovieDB);
